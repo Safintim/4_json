3f0c0a941f86bf5d686331984bf17dce66b78226
Safin Timur <timurtlt96@mail.ru>
2019-07-16 20:45:59 +0300

    Added check extension in paarse

--- pprint_json.py
+++ pprint_json.py
@@ -2,10 +2,23 @@ import json
 import argparse
 
 
+def main():
+    parser = create_parser()
+    namespace = parser.parse_args()
+    if not namespace.file:
+        exit('Укажите путь к файлу. Формат файла должен быть json')
+    content_json_file = load_data(namespace.file)
+    print(pretty_print_json(content_json_file))
+
+
 def create_parser():
     parser = argparse.ArgumentParser(prefix_chars='-+/')
-    parser.add_argument('file', nargs='?', default=False, help='Path to json file')
+    parser.add_argument('file', nargs='?', default=False, type=is_json, help='Path to json file')
     return parser
+
+
+def is_json(file):
+    return file.lower().endswith('.json')
 
 
 def load_data(filepath):
@@ -15,15 +28,6 @@ def load_data(filepath):
 
 def pretty_print_json(json_content):
     return json.dumps(json_content, indent=4, ensure_ascii=False, sort_keys=True)
-
-
-def main():
-    parser = create_parser()
-    namespace = parser.parse_args()
-    if not namespace.file or not namespace.file.lower().endswith('.json'):
-        exit('Укажите путь к файлу. Формат файла должен быть json')
-    content_json_file = load_data(namespace.file)
-    print(pretty_print_json(content_json_file))
 
 
 if __name__ == '__main__':
