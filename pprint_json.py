05c235463d7dfb7f09f53b0988a94cbebe6f3ee9
Safin Timur <timurtlt96@mail.ru>
2019-07-18 10:28:56 +0300

    Move check args in Parser

--- pprint_json.py
+++ pprint_json.py
@@ -5,21 +5,20 @@ import argparse
 def main():
     parser = create_parser()
     namespace = parser.parse_args()
-    if not namespace.file:
-        exit('Укажите путь к файлу. Формат файла должен быть json')
     content_json_file = load_data(namespace.file)
     print(pretty_print_json(content_json_file))
 
 
 def create_parser():
     parser = argparse.ArgumentParser(prefix_chars='-+/')
-    parser.add_argument('file', nargs='?', default=False, type=is_json, help='Path to json file')
+    parser.add_argument('file', type=is_json, help='Path to json file')
     return parser
 
 
 def is_json(filepath):
     if filepath.lower().endswith('.json'):
         return filepath
+    raise argparse.ArgumentTypeError('invalid file format')
 
 
 def load_data(filepath):
